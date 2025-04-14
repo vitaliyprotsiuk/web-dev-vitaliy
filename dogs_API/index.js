f8987cf1ce91a14de5d4e2a68a12ac3ead38b8f6
vitaliyprotsiuk <xiz60606@gmail.com>
2025-04-15 00:47:04 +0300

    Add previuos image button

--- dogs_API/index.js
+++ dogs_API/index.js
@@ -1,4 +1,6 @@
 const APILink = "https://dog.ceo/api/breeds/image/random";
+let imageLinks = [];
+let currentImgNumber = -1;
 
 const fetchURL = async function() {
     const response = await fetch(APILink);
@@ -12,25 +14,62 @@ const fetchURL = async function() {
     const status = responseJSON.status;
 
     if (status == 'success'){
+        imageLinks.push(responseJSON.message)
         return responseJSON.message;
     };
 };
 
-const showIMG = async function() {
-    const link = await fetchURL();
+const updateMainDiv = function(htmlCode) {
+    $('#main').html(`${htmlCode}`);
+};
 
-    if (link) {
-        $('#main').append(`
-            <div class="text">Випадкове фото собаки</div>
-            <div class='ramka-7'><img class='ramka-7' src="${link}" alt="dog" height='500' wight='500' style='object-fit: cover;'></div>
-        `)
+const manageBackButton = function(OK) {
+    if (OK) {
+        $('#back').html('<button onclick="showPrevImage()">Назад</button>');
+    } else {
+        $('#back').html('');
     };
 };
 
-showIMG();
+const showImage = function(link) {
+    updateMainDiv(`
+        <div class="text">Випадкове фото собаки</div>
+        <div class='ramka-7'>
+            <img class='ramka-7' src="${link}" alt="dog" height='500' width='500' style='object-fit: cover;'>
+        </div>
+    `)
+};
 
-const changeIMG = async function() {
-    $('#main').html('');
+const showNextImage = async function() {
+    currentImgNumber++;
 
-    await showIMG();
-}
+    if (currentImgNumber == imageLinks.length) {
+        link = await fetchURL();
+    } else {
+        link = imageLinks[currentImgNumber]
+    };
+
+    if (link) {
+        updateMainDiv('');
+        showImage(link)
+    };
+    
+    if (currentImgNumber > 0) {
+        manageBackButton(true)
+    }
+};
+
+const showPrevImage = async function() {
+    currentImgNumber--;
+    if (currentImgNumber <= 0) {
+        manageBackButton(false)
+        link = imageLinks[currentImgNumber];
+        showImage(link);
+        currentImgNumber = 0;
+    } else {
+        link = imageLinks[currentImgNumber];
+        showImage(link);
+    };
+};
+
+showNextImage();
